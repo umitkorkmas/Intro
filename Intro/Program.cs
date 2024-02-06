5730d5f68dd78dc41d367b648843f60c62cb0e52
umitk <umitk@UMITKORKMAZ>
2024-02-06 23:32:50 +0300

    Add project files.

---
+++ Intro/Program.cs
@@ -0,0 +1,38 @@
+﻿using Intro.Business;
+using Intro.Entitites;
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000;
+
+
+//variables -- camelCase
+bool isAuthanticated = true;
+//condition 
+if (isAuthanticated == true)
+{
+    Console.WriteLine("Hoşgeldin Engin");
+}
+else
+{
+    Console.WriteLine("Buton --> Giriş Yap butonu");
+}
+
+string[] loans = {"Kredi1", "Kredi2", "Kredi3", "Kredi4", "Kredi5", "Kredi6"};
+//string[] loans2 = new string[6];
+//loans[0] = "kredi1";
+
+         //start     //condition  //increment  
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+
+CourseManager courseManager = new CourseManager();
+Course[] courses2 = courseManager.GetAll();
+
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name);
+}
